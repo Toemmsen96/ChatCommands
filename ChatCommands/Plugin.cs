0dd3aac5534b93cfc5d69c49262c7b3448e3a6d9
Toemmsen96 <hotztom2@gmail.com>
2024-02-20 13:25:36 +0100

    i try networking lol

--- ChatCommands/Plugin.cs
+++ ChatCommands/Plugin.cs
@@ -17,6 +17,7 @@ using System.Net;
 using static System.Net.Mime.MediaTypeNames;
 using System.Xml.Linq;
 using System.Text.RegularExpressions;
+using Networking;
 
 namespace ChatCommands
 {
@@ -326,41 +327,16 @@ namespace ChatCommands
         }
 
 
-        //internal static void SendHostCommand(string commandInput)
-        //{
-        //    if (!isHost || !SendHostCommandsSetting.Value)
-        //    {
-        //        return;
-        //    }
-        //    string commandToClients = ChatCommands.NetHostCommandPrefix + commandInput + ChatCommands.NetCommandPostfix;
-        //    HUDManager.Instance.AddTextToChatOnServer(commandToClients, -1);
-        //}
-        // Method to send a command from the host to all clients
         internal static void SendHostCommand(string commandInput)
         {
             if (!isHost || !SendHostCommandsSetting.Value)
             {
                 return;
             }
-
-            // Construct the full command to send to clients
-            string commandToClients = NetHostCommandPrefix + commandInput + NetCommandPostfix;
-
-            // Call an RPC to execute the command on all clients
-            instance.RpcExecuteCommandOnClients(commandToClients);
+            string commandToClients = ChatCommands.NetHostCommandPrefix + commandInput + ChatCommands.NetCommandPostfix;
+            HUDManager.Instance.AddTextToChatOnServer(commandToClients, -1);
         }
-
-        // RPC method to execute the command on all clients
-        [ClientRpc]
-        void RpcExecuteCommandOnClients(string command)
-        {
-            // Assuming you have a method to handle adding text to the chat on clients
-            // You would replace this line with the appropriate method call to add text to the chat
-            ProcessNetHostCommand(command);
-        }
-
-
-
+        
         public static void ProcessNetHostCommand(string commandInput)
         {
             if (commandInput.ToLower().Contains("god"))
